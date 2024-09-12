675901c442472a4173c06b291ea531b8f4577ac9
GujjarlapudiSandeep <sandeepgujjarlapudi28@gmail.com>
2024-09-12 15:10:41 +0530

    added js and code

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelector("#form").addEventListener("submit", jobsubmit);
+let jobArray;
+if (localStorage.getItem("joblist") !== null) {
+  jobArray = JSON.parse(localStorage.getItem("joblist"));
+} else {
+  jobArray = [];
+}
+
+function jobsubmit() {
+  event.preventDefault();
+  let jobObject = {
+    personName: document.querySelector("#name").value,
+    personEmail: form.email.value,
+    personRole: form.role.value,
+    personSalary: form.salary.value,
+  };
+
+  jobArray.push(jobObject);
+  localStorage.setItem("joblist", JSON.stringify(jobArray));
+}
